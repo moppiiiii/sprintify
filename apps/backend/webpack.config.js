26f7d09886ded09ac6b23213084535e7fc678410
moppiiiii <tnktmhssrc@gmail.com>
2024-09-12 19:35:41 +0900

    chore(Biome): add Biome

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,20 +1,20 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
+const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
+const { join } = require("path");
 
 module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/backend'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-    }),
-  ],
+	output: {
+		path: join(__dirname, "../../dist/apps/backend"),
+	},
+	plugins: [
+		new NxAppWebpackPlugin({
+			target: "node",
+			compiler: "tsc",
+			main: "./src/main.ts",
+			tsConfig: "./tsconfig.app.json",
+			assets: ["./src/assets"],
+			optimization: false,
+			outputHashing: "none",
+			generatePackageJson: true,
+		}),
+	],
 };
