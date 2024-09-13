50bbfba02c30854d386750949c7907d4c933777a
moppiiiii <tnktmhssrc@gmail.com>
2024-09-13 17:29:01 +0900

    fix(Biome/settings): change Biome settings

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -2,19 +2,19 @@ const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
 const { join } = require("path");
 
 module.exports = {
-	output: {
-		path: join(__dirname, "../../dist/apps/backend"),
-	},
-	plugins: [
-		new NxAppWebpackPlugin({
-			target: "node",
-			compiler: "tsc",
-			main: "./src/main.ts",
-			tsConfig: "./tsconfig.app.json",
-			assets: ["./src/assets"],
-			optimization: false,
-			outputHashing: "none",
-			generatePackageJson: true,
-		}),
-	],
+  output: {
+    path: join(__dirname, "../../dist/apps/backend"),
+  },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: "node",
+      compiler: "tsc",
+      main: "./src/main.ts",
+      tsConfig: "./tsconfig.app.json",
+      assets: ["./src/assets"],
+      optimization: false,
+      outputHashing: "none",
+      generatePackageJson: true,
+    }),
+  ],
 };
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -4,11 +4,11 @@ import styles from "./app.module.scss";
 import NxWelcome from "./nx-welcome";
 
 export function App() {
-	return (
-		<div>
-			<NxWelcome title="frontend" />
-		</div>
-	);
+  return (
+    <div>
+      <NxWelcome title="frontend" />
+    </div>
+  );
 }
 
 export default App;
