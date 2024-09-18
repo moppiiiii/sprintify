fb46974fc550465e56c8b2bd940db22530ae6cee
moppiiiii <tnktmhssrc@gmail.com>
2024-09-18 21:51:53 +0900

    feat(list): create list sample

--- apps/frontend/src/app/components/layouts/usually/Usually.layout.tsx
+++ apps/frontend/src/app/components/layouts/usually/Usually.layout.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import HeaderComponent from "../../parts/header/Header.parts";
 import NavbarComponent from "../../parts/navbar/Navbar.parts";
 import { UsuallyLayoutProps } from "./type";
-import { Group } from "@mantine/core";
+
+import classes from "./Usually.layout.module.scss";
 
 const UsuallyLayout: React.FC<UsuallyLayoutProps> = ({ children }) => {
   return (
-    <main>
+    <main className={classes["usually-layout-container"]}>
       <HeaderComponent />
-      <Group gap={0}>
-        <NavbarComponent />
-        {children}
-      </Group>
+      <div className={classes["layout-grid"]}>
+        <div className={classes["navbar-col"]}>
+          <NavbarComponent />
+        </div>
+        <div>{children}</div>
+      </div>
     </main>
   );
 };
